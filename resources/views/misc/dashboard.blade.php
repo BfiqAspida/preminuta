386d3d097c32ceda3dda77008134de6c162ed238
Luis Alejandro Castro Vidal <cio@aspida.com.co>
2024-04-02 07:59:55 -0500

    First Commit

--- resources/views/misc/dashboard.blade.php
+++ resources/views/misc/dashboard.blade.php
@@ -1,14 +1,8 @@
 @extends('layout')
 
 @section('content')
-@auth
-        <h1>Dashboard</h1>
-        <p class="lead">Only authenticated users can access this section.</p>
-        <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View more tutorials here &raquo;</a>
-        @endauth
 
-        @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
-        @endguest
+<div>
+        <br><br><br>
+</div>
 @endsection
