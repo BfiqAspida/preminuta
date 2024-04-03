13b338b684633c0f9868c5b23211e2913d8b1a55
Luis Alejandro Castro Vidal <cio@aspida.com.co>
2024-04-03 07:13:57 -0500

    Vehicles

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -24,7 +24,7 @@ class VehicleController extends Controller
             //$id=1;
             //$workstation=1;
             $name = $request->session()->get('user')["name"];
-            $vehicles = Vehicle::where('Vehicles_vehicles_book.WorkStation_id_id', 'd842ea84-a431-11ee-a506-0242ac120002')->get();
+            $vehicles = Vehicle::whereIn('Vehicles_vehicles_book.WorkStation_id_id', , ['d842ea84-a431-11ee-a506-0242ac120002', '0977e274-a425-11ee-a506-0242ac120002', '5c0b11c6-8d6b-11ee-b9d1-0242ac120002'])->get();
             return view('misc.vehicles',compact('vehicles', 'id', 'workstation', 'inicial', 'final','name'));
         }
         else
