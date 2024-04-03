f1654558e8adc3561c7d03ff5db88ecdb0d80a95
Luis Alejandro Castro Vidal <cio@aspida.com.co>
2024-04-03 07:12:48 -0500

    Vehicles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
             $name = $request->session()->get('user')["name"];
             $project = $request->session()->get('user')["project_id"];
             $vehicles = Vehicle::select(DB::raw('DATE(created_at) as date'), DB::raw('count(id) as vehiculos'))
-                ->whereIn('Vehicles_vehicles_book.WorkStation_id_id', ['d842ea84-a431-11ee-a506-0242ac120002', '0977e274-a425-11ee-a506-0242ac120002'])
+                ->whereIn('Vehicles_vehicles_book.WorkStation_id_id', ['d842ea84-a431-11ee-a506-0242ac120002', '0977e274-a425-11ee-a506-0242ac120002', '5c0b11c6-8d6b-11ee-b9d1-0242ac120002'])
                 ->where('Vehicles_vehicles_book.created_at', '>', '2024-04-01')
                 ->groupBy('date')
                 ->orderBy('date')
